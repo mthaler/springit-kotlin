31c9901cf7655efa968b50ba6206539319d79e35
Michael Thaler <michael.thaler@dlr.de>
2021-04-04 09:43:53 +0200

    Added home.html.

--- src/main/kotlin/com/mthaler/springit/controller/HomeController.kt
+++ src/main/kotlin/com/mthaler/springit/controller/HomeController.kt
@@ -1,13 +1,15 @@
 package com.mthaler.springit.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class HomeController {
 
-//    @RequestMapping("/")
-//    fun home(): String {
-//        return "Hello, Spring Boot 2!"
-//    }
+    @RequestMapping("/home")
+    fun home(model: Model): String {
+        model.addAttribute("title", "Hello, Thymeleaf!")
+        return "home"
+    }
 }
