97eb844cd40e97a382a81f59644d40fed8f9ace9
Michael Thaler <michael.thaler@dlr.de>
2021-04-04 10:58:52 +0200

    Added some methods to Link class.

--- src/main/kotlin/com/mthaler/springit/domain/Link.kt
+++ src/main/kotlin/com/mthaler/springit/domain/Link.kt
@@ -1,14 +1,21 @@
 package com.mthaler.springit.domain
 
+import com.mthaler.springit.service.BeanUtil
 import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedBy
 import org.springframework.data.annotation.LastModifiedDate
+import java.net.URI
 import java.time.LocalDateTime
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.OneToMany
+import org.ocpsoft.prettytime.PrettyTime
+import java.time.ZoneId
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 @Entity
 class Link(
@@ -20,4 +27,21 @@ class Link(
     @CreatedDate var creationDate: LocalDateTime? = null,
     @LastModifiedBy var lastModifiedBy: String? = null,
     @LastModifiedDate var lastModifiedDate: LocalDateTime? = null
-)
+) {
+    fun getDomainName(): String {
+        val uri = URI(url)
+        val domain: String = uri.getHost()
+        return if (domain.startsWith("www.")) domain.substring(4) else domain
+    }
+
+    fun getPrettyTime(): String? {
+        return creationDate?.let {
+            val pt: PrettyTime = BeanUtil.getBean(PrettyTime::class.java)
+            pt.format(convertToDateViaInstant(it))
+        } ?: null
+    }
+
+    private fun convertToDateViaInstant(dateToConvert: LocalDateTime): Date {
+        return Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant())
+    }
+}
