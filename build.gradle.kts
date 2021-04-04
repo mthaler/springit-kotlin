bff69202aa80a50cdaeed758e3d801e53b8df5ab
Michael Thaler <michael.thaler@dlr.de>
2021-04-04 10:40:59 +0200

    Added prettytime dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.ocpsoft.prettytime:prettytime:5.0.0.Final")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
