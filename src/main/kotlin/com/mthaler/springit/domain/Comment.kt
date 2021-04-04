e5e403470301451d50d083cb441224ac821a9f2a
Michael Thaler <michael.thaler@dlr.de>
2021-04-04 07:52:21 +0200

    Updated Comment class.

--- src/main/kotlin/com/mthaler/springit/domain/Comment.kt
+++ src/main/kotlin/com/mthaler/springit/domain/Comment.kt
@@ -11,10 +11,12 @@ import javax.persistence.Id
 import javax.persistence.ManyToOne
 
 @Entity
-class Comment(@Id @GeneratedValue val id: Long,
-                   val body: String,
-                   @ManyToOne val link: Link?,
-                   @CreatedBy val createdBy: String,
-                   @CreatedDate val creationDate: LocalDateTime,
-                   @LastModifiedBy val lastModifiedBy: String,
-                   @LastModifiedDate val lastModifiedDate: LocalDateTime)
+class Comment(
+    val body: String,
+    @ManyToOne val link: Link?,
+    @CreatedBy val createdBy: String,
+    @Id @GeneratedValue val id: Long? = null,
+    @CreatedDate val creationDate: LocalDateTime? = null,
+    @LastModifiedBy val lastModifiedBy: String? = null,
+    @LastModifiedDate val lastModifiedDate: LocalDateTime? = null
+)
