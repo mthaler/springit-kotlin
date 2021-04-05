4474f1ff43f74706e6eae3f103392896e93147cb
Michael Thaler <michael.thaler@dlr.de>
2021-04-05 14:23:40 +0200

    Updated AuthController.

--- src/main/kotlin/com/mthaler/springit/controller/AuthController.kt
+++ src/main/kotlin/com/mthaler/springit/controller/AuthController.kt
@@ -1,10 +1,11 @@
 package com.mthaler.springit.controller
 
+import com.mthaler.springit.service.UserService
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class AuthController {
+class AuthController(val userService: UserService) {
 
     @GetMapping("/login")
     fun login(): String {
