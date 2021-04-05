17429d18e5be94d014f6637719e59e5c361c7901
Michael Thaler <michael.thaler@dlr.de>
2021-04-05 16:27:55 +0200

    VoteController now uses VoteService and LinkService.

--- src/main/kotlin/com/mthaler/springit/controller/VoteController.kt
+++ src/main/kotlin/com/mthaler/springit/controller/VoteController.kt
@@ -5,25 +5,27 @@ import com.mthaler.springit.repository.LinkRepository
 import com.mthaler.springit.repository.VoteRepository
 import org.springframework.web.bind.annotation.RestController
 import com.mthaler.springit.domain.Vote
+import com.mthaler.springit.service.LinkService
+import com.mthaler.springit.service.VoteService
 import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.GetMapping
 import java.util.*
 
 @RestController
-class VoteController(val voteRepository: VoteRepository, val linkRepository: LinkRepository) {
+class VoteController(val voteService: VoteService, val linkService: LinkService) {
 
     @Secured(value = ["ROLE_USER"])
     @GetMapping("/vote/link/{linkID}/direction/{direction}/votecount/{voteCount}")
     fun vote(@PathVariable linkID: Long?, @PathVariable direction: Short, @PathVariable voteCount: Int): Int {
-        val optionalLink: Optional<Link> = linkRepository.findById(linkID!!)
+        val optionalLink: Optional<Link> = linkService.findById(linkID!!)
         if (optionalLink.isPresent()) {
             val link: Link = optionalLink.get()
             val vote = Vote(direction, link)
-            voteRepository.save(vote)
+            voteService.save(vote)
             val updatedVoteCount = voteCount + direction
             link.voteCount = updatedVoteCount
-            linkRepository.save<Link>(link)
+            linkService.save(link)
             return updatedVoteCount
         }
         return voteCount
