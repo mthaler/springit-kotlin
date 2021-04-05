ba6f0c198016bf3e777068bf599b0560641792a4
Michael Thaler <michael.thaler@dlr.de>
2021-04-05 09:44:24 +0200

    Comment now extends Auditable.

--- src/main/kotlin/com/mthaler/springit/domain/Comment.kt
+++ src/main/kotlin/com/mthaler/springit/domain/Comment.kt
@@ -1,10 +1,5 @@
 package com.mthaler.springit.domain
 
-import org.springframework.data.annotation.CreatedBy
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.LastModifiedBy
-import org.springframework.data.annotation.LastModifiedDate
-import java.time.LocalDateTime
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
@@ -14,9 +9,5 @@ import javax.persistence.ManyToOne
 class Comment(
     var body: String,
     @ManyToOne var link: Link?,
-    @Id @GeneratedValue var id: Long? = null,
-    @CreatedBy var createdBy: String? = null,
-    @CreatedDate var creationDate: LocalDateTime? = null,
-    @LastModifiedBy var lastModifiedBy: String? = null,
-    @LastModifiedDate var lastModifiedDate: LocalDateTime? = null
-)
+    @Id @GeneratedValue var id: Long? = null
+): Auditable()
