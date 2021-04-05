537a9d332f8b07046d40081ed099a21330d3563f
Michael Thaler <michael.thaler@dlr.de>
2021-04-05 09:46:02 +0200

    Vote now extends Auditable.

--- src/main/kotlin/com/mthaler/springit/domain/Vote.kt
+++ src/main/kotlin/com/mthaler/springit/domain/Vote.kt
@@ -1,10 +1,5 @@
 package com.mthaler.springit.domain
 
-import org.springframework.data.annotation.CreatedBy
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.LastModifiedBy
-import org.springframework.data.annotation.LastModifiedDate
-import java.time.LocalDateTime
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
@@ -12,9 +7,5 @@ import javax.persistence.Id
 @Entity
 class Vote(
     var vote: Int,
-    @Id @GeneratedValue var id: Long? = null,
-    @CreatedBy var createdBy: String? = null,
-    @CreatedDate var creationDate: LocalDateTime? = null,
-    @LastModifiedBy var lastModifiedBy: String? = null,
-    @LastModifiedDate var lastModifiedDate: LocalDateTime? = null
-)
+    @Id @GeneratedValue var id: Long? = null
+): Auditable()
