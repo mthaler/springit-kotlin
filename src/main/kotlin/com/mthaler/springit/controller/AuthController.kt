b11f652bb908b7190b67a597357e7ead04a04853
Michael Thaler <michael.thaler@dlr.de>
2021-04-05 10:54:29 +0200

    Updated AuthController.

--- src/main/kotlin/com/mthaler/springit/controller/AuthController.kt
+++ src/main/kotlin/com/mthaler/springit/controller/AuthController.kt
@@ -3,14 +3,21 @@ package com.mthaler.springit.controller
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
-
-
-
 @Controller
 class AuthController {
 
     @GetMapping("/login")
     fun login(): String {
         return "auth/login"
+    }
+
+    @GetMapping("/profile")
+    fun profile(): String {
+        return "auth/profile"
+    }
+
+    @GetMapping("/register")
+    fun register(): String {
+        return "auth/register"
     }
 }
