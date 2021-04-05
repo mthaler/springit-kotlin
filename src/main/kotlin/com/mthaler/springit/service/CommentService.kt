a61aa16d341fb8b80dffd05e068961a53692655d
Michael Thaler <michael.thaler@dlr.de>
2021-04-05 16:20:32 +0200

    Added CommentService.

---
+++ src/main/kotlin/com/mthaler/springit/service/CommentService.kt
@@ -0,0 +1,13 @@
+package com.mthaler.springit.service
+
+import com.mthaler.springit.domain.Comment
+import com.mthaler.springit.repository.CommentRepository
+import org.springframework.stereotype.Service
+
+@Service
+class CommentService(val commentRepository: CommentRepository) {
+    
+    fun save(comment: Comment): Comment {
+        return commentRepository.save(comment)
+    }
+}
