7e8758a0b95428e7eb747e22ba20acbe8bc824e7
Michael Thaler <michael.thaler@dlr.de>
2021-04-05 11:12:06 +0200

    Updated Vote class.

--- src/main/kotlin/com/mthaler/springit/domain/Vote.kt
+++ src/main/kotlin/com/mthaler/springit/domain/Vote.kt
@@ -3,9 +3,12 @@ package com.mthaler.springit.domain
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.ManyToOne
 
 @Entity
 class Vote(
-    var vote: Int,
+    var direction: Short,
+    @ManyToOne
+    var link: Link,
     @Id @GeneratedValue var id: Long? = null
 ): Auditable()
